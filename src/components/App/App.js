ef10722c7a824fa0961daca5aa7e177a650b443e
zackcarlson <zackt.carlson@gmail.com>
2020-10-09 20:20:25 -0700

    Add linting

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,12 @@
-import React, { Component } from 'react';
-import './App.css';
+import React from "react"
+import "./App.css"
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <h2>New Engen</h2>
-        </div>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="App">
+    <div className="App-header">
+      <h2>Hey World</h2>
+    </div>
+  </div>
+)
 
-export default App;
+export default App
