ce455f739820872b7ef1e204dbb636e3f9881174
zackcarlson <zackt.carlson@gmail.com>
2020-10-09 17:36:54 -0700

    Add heroku app to remote

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,7 +6,7 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <h2>Welcome to React</h2>
+          <h2>New Engen</h2>
         </div>
       </div>
     );
