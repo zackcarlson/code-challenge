0991eafd5eafab19d2f7de2910e98148c576c544
zackcarlson <zackt.carlson@gmail.com>
2020-10-12 21:24:42 -0700

    Adds SAVE and DELETE cases to paletteContext

--- src/state/context/paletteContext.js
+++ src/state/context/paletteContext.js
@@ -1,5 +1,6 @@
+import { v4 as uuidv4 } from "uuid";
 import makeStore from "../store";
-import SAVE_PALETTE from "../constants";
+import { SAVE_PALETTE, DELETE_PALETTE } from "../constants";
 
 const initalState = JSON.parse(window.localStorage.getItem("palettes")) || [];
 
@@ -7,7 +8,26 @@ const reducer = (state, action) => {
   switch (action.type) {
     case SAVE_PALETTE: {
       const { paletteName, palette } = action;
-      return [...state, { paletteName, palette }];
+      const palettes = [...state, { paletteName, palette, id: uuidv4() }];
+
+      window.localStorage.setItem("palettes", JSON.stringify(palettes));
+      return palettes;
+    }
+    case DELETE_PALETTE: {
+      const { id } = action;
+      const palettes = state.filter((info) => {
+        if (info.id !== id) {
+          const temp = {};
+          temp.paletteName = info.paletteName;
+          temp.palette = [...info.palette];
+          temp.id = info.id;
+          return temp;
+        }
+        return "";
+      });
+
+      window.localStorage.setItem("palettes", JSON.stringify(palettes));
+      return palettes;
     }
     default:
       return state;
