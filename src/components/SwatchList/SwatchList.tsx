7bd05385bf444716307de96c1f73573047553228
zackcarlson <zackt.carlson@gmail.com>
2020-10-12 21:18:13 -0700

    Adds types and modularizes SwatchList

--- src/components/SwatchList/SwatchList.tsx
+++ src/components/SwatchList/SwatchList.tsx
@@ -1,30 +1,37 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Swatch from "../Swatch/Swatch";
-import { useCartStore } from "../../state/context/cartContext";
+import { useCartStore, useCartDispatch } from "../../state/context/cartContext";
+import { TOGGLE_SWATCH } from "../../state/constants";
 import "./SwatchList.css";
-
-interface SwatchInfo {
-  hex: string;
-  __typename: string;
-}
-
-type SwatchListProps = {
-  swatches: SwatchInfo[],
-};
+import { SwatchListProps } from "../../types";
 
 const SwatchList = ({ swatches }: SwatchListProps) => {
   const cart = new Set(useCartStore());
+  const dispatch = useCartDispatch();
+
+  const handleToggle = (hexCode: string): void => {
+    dispatch({ type: TOGGLE_SWATCH, swatch: hexCode });
+  };
 
   return (
     <ul className="swatch-list">
-      {swatches && swatches.length > 0 && swatches.map(({ hex }) => {
-        let isSelected = false;
+      {swatches &&
+        swatches.length > 0 &&
+        swatches.map(({ hex }) => {
+          let isSelected: boolean = false;
 
-        if (cart.has(hex)) isSelected = true;
+          if (cart.has(hex)) isSelected = true;
 
-        return <Swatch key={hex} hex={hex} isSelected={isSelected} />;
-      })}
+          return (
+            <Swatch
+              key={hex}
+              hex={hex}
+              isSelected={isSelected}
+              handleClick={() => handleToggle(hex)}
+            />
+          );
+        })}
     </ul>
   );
 };
