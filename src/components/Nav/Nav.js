ac7e678edf9a42d324811849ec2b1bc136d2e9e7
zackcarlson <zackt.carlson@gmail.com>
2020-10-13 23:22:16 -0700

    Adds nav styling and cart icon amount styles

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -7,20 +7,33 @@ import logo from "../../assets/icons/NewEngen-Logo.svg";
 
 const Nav = () => {
   const cart = useCartStore();
+
+  const generateCartAmountStyle = () => {
+    let cartAmountStyle = "";
+    const cartLength = cart.length;
+
+    if (cartLength === 10) cartAmountStyle = "ten";
+    else if (cartLength < 10) cartAmountStyle = "less-than-ten";
+    else cartAmountStyle = "greater-than-ten";
+    return cartAmountStyle;
+  };
+
   return (
     <nav className="nav-bar">
       <ul>
-        <li>
+        <li className="nav-item logo-li-item">
           <Link to="/" className="logo-link">
             <img src={logo} alt="New Engen Logo" className="logo-icon" />
           </Link>
         </li>
-        <li>
+        <li className="nav-item cart-li-item">
           <Link to="/cart" className="cart-link">
             <img src={cartIcon} alt="Cart Icon" className="cart-icon" />
-            <span className="cart-amount">
-              {cart && cart.length > 0 && cart.length}
-            </span>
+            {cart && cart.length > 0 && (
+              <span className={`cart-amount ${generateCartAmountStyle()}`}>
+                {cart.length > 10 ? "10+" : cart.length}
+              </span>
+            )}
           </Link>
         </li>
       </ul>
