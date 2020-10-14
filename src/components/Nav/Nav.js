40342a4d0ab6638cdff24b408684d122bca2d3ca
zackcarlson <zackt.carlson@gmail.com>
2020-10-14 14:40:14 -0700

    Converts icon images to svg components

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -2,8 +2,8 @@ import React from "react";
 import "./Nav.css";
 import { Link } from "react-router-dom";
 import { useCartStore } from "../../state/context/cartContext";
-import cartIcon from "../../assets/icons/CartIcon.svg";
-import logo from "../../assets/icons/NewEngen-Logo.svg";
+import LogoIcon from "../LogoIcon/LogoIcon";
+import CartIcon from "../CartIcon/CartIcon";
 
 const Nav = () => {
   const cart = useCartStore();
@@ -23,17 +23,17 @@ const Nav = () => {
       <ul>
         <li className="nav-item logo-li-item">
           <Link to="/" className="logo-link">
-            <img src={logo} alt="New Engen Logo" className="logo-icon" />
+            <LogoIcon />
           </Link>
         </li>
         <li className="nav-item cart-li-item">
           <Link to="/cart" className="cart-link">
-            <img src={cartIcon} alt="Cart Icon" className="cart-icon" />
             {cart && cart.length > 0 && (
               <span className={`cart-amount ${generateCartAmountStyle()}`}>
                 {cart.length > 10 ? "10+" : cart.length}
               </span>
             )}
+            <CartIcon />
           </Link>
         </li>
       </ul>
