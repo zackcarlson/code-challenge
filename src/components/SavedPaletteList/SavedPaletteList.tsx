795387002cf39b4e834e21e01d9a1e6e5e74c9c7
zackcarlson <zackt.carlson@gmail.com>
2020-10-13 23:29:03 -0700

    Modularizes component, imports new SavedPalette component, adds additional jsx structure and basic styles

--- src/components/SavedPaletteList/SavedPaletteList.tsx
+++ src/components/SavedPaletteList/SavedPaletteList.tsx
@@ -1,31 +1,45 @@
 import React from "react";
-import Swatch from "../Swatch/Swatch";
+import PropTypes from "prop-types";
 import { PaletteType, SavedPaletteListProps } from "../../types";
+import SavedPalette from "../SavedPalette/SavedPalette";
+import "./SavedPaletteList.css";
 
 const SavedPaletteList = ({
   palettes,
   handleDeletePalette,
 }: SavedPaletteListProps) => {
   return (
-    <ul className="saved-palette-list">
-      {palettes &&
-        palettes.length > 0 &&
-        palettes.map((paletteInfo: PaletteType) => {
-          const { paletteName, palette, id } = paletteInfo;
-          return (
-            <div key={`${id}-${paletteName}`}>
-              <h4>{paletteName}</h4>
-              {palette.map((hex) => (
-                <Swatch key={hex} hex={hex} />
-              ))}
-              <button type="button" onClick={() => handleDeletePalette(id)}>
-                DELETE
-              </button>
-            </div>
-          );
-        })}
-    </ul>
+    <div className="previous-palettes">
+      <h3>Previously saved color palettes</h3>
+      <div className="saved-palette-list">
+        {palettes && palettes.length > 0 ? (
+          palettes.map((paletteInfo: PaletteType) => {
+            const { id, paletteName } = paletteInfo;
+            return (
+              <SavedPalette
+                paletteInfo={paletteInfo}
+                handleDeletePalette={handleDeletePalette}
+                key={`${id}-${paletteName}`}
+              />
+            );
+          })
+        ) : (
+          <p>You haven&apos;t saved any palettes, yet.</p>
+        )}
+      </div>
+    </div>
   );
+};
+
+SavedPaletteList.propTypes = {
+  palettes: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string,
+      paletteName: PropTypes.string,
+      palette: PropTypes.arrayOf(PropTypes.string),
+    })
+  ).isRequired,
+  handleDeletePalette: PropTypes.func.isRequired,
 };
 
 export default SavedPaletteList;
